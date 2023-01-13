00e00bd58eeda5da418abc59213e26960797e74d
imranyy <imranmatano6@gmail.com>
2023-01-13 11:56:23 +0300

    cleaning

---
+++ components/navbar.js
@@ -0,0 +1,13 @@
+const navbar=()=> {
+  return(
+    `<nav>
+        <ul>
+            <li><a href="index.html">Home</a></li>
+            <li><a href="index.html">Home</a></li>
+            <li><a href="index.html">Home</a></li>
+            <li><a href="index.html">Home</a></li>
+        </ul>
+    </nav>`
+  )
+}
+export default navbar;
--- main.js
+++ main.js
@@ -1,22 +1,17 @@
-import './style.css'
-import { setupCounter } from './counter.js'
+import './css/style.css'
+import './css/responsive.css'
+// import navbar from './components/navbar.js'
 
 document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="/" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
+  <div class='body'>
+    <nav>
+      <ul role='navigation'>
+          <li><a href="index.html">Home</a></li>
+          <li><a href="index.html">Home</a></li>
+          <li><a href="index.html">Home</a></li>
+          <li><a href="index.html">Home</a></li>
+      </ul>
+    </nav>
   </div>
 `
 
-setupCounter(document.querySelector('#counter'))
