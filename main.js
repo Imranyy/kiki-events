16532dacecf96de9b3271ec8850dc6918f856c23
imranyy <imranmatano6@gmail.com>
2023-01-13 17:27:32 +0300

    fixing some error

--- main.js
+++ main.js
@@ -22,15 +22,16 @@ document.querySelector('#app').innerHTML = `
         </nav>
 
        <div class="landing">
-        <div class='hero-image'>
-            <h1>Welcome to <span style="color:pink;">Kiki</span> Events</h1>
-            <h3>We handle event planning and organising.</h3>
-            <button class="book-btn">Book Now</button>
-        </div>
+            <div class='hero-image'>
+                <h1>Welcome to <span style="color:pink;">Kiki</span> Events</h1>
+                <h3>We handle event planning and organising.</h3>
+                <button class="book-btn">Book Now</button>
+            </div>
 
-        <div class="about">
-            <div class="content">
-                <img src="./assests/img1.jpeg" alt="kiki event image"/>
+            
+
+            <div class="about">
+                <img src="./assests/img2.jpeg" alt="kiki event image"/>
                 <div class="text">
                     <h2 style="color: rgb(211, 73, 96);">About Us</h2>
                     <p>
@@ -38,10 +39,20 @@ document.querySelector('#app').innerHTML = `
                         Need ideas? We’ve collected some amazing examples of party websites from our global community of designers. 
                         Get inspired and start planning the perfect party web design today.
                     </p>
-                <div>
-             </div>
-        </div>
+                </div>
+            </div>   
 
-       </div>
+            <div class="about">
+                <div class="text">
+                    <h2 style="color: rgb(211, 73, 96);">Our History</h2>
+                    <p>
+                        Make your business shine online with a custom party website designed just for you by a professional designer. 
+                        Need ideas? We’ve collected some amazing examples of party websites from our global community of designers. 
+                        Get inspired and start planning the perfect party web design today.
+                    </p>
+                </div>
+                <img src="./assests/img4.jpeg" alt="kiki event image"/>
+            </div>   
+        </div>
     </div>
 `
